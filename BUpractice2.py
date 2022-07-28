16ec4365889852c52d3c0a78e95bebe81eb3d38d
PeakSkeie <jerryzgj@gmail.com>
2022-07-28 15:48:14 -0400

    update

--- BUpractice2.py
+++ BUpractice2.py
@@ -13,3 +13,15 @@ area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
 
 plt.scatter(x, y, s=area, c=colors, alpha=0.5)
 plt.show()
+
+class person:
+    apple = 1
+    def _init_(self, name, age):
+        self.name = name
+        self.age = age
+
+def myfunc(self):
+    print("Hi my name is " + self.name)
+
+p1 = person("Jphn", 36)
+p1.myfunc() 
