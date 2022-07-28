1478138e935dd167ab3c7a71db3cc8be155b7f96
PeakSkeie <jerryzgj@gmail.com>
2022-07-28 15:48:58 -0400

    new

---
+++ Week1_Python_Project_demo.py
@@ -0,0 +1,25 @@
+print("Demo Start!")
+
+class user_details:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.friendlist = []
+        self.messages = []
+        self.blocked_messages = []
+    
+    def edit(self):
+        print("edit")
+        
+
+#friends
+#    add
+#    view_all
+#    block
+#    send_message
+#    check_message
+
+#a = 
+
+user_details.edit(1)
+
